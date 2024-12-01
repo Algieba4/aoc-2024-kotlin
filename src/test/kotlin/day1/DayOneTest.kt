b3245594c68149eec256944fd6071bf46d5be6ea
Travis O <travisottelien@gmail.com>
2024-12-01 17:26:10 -0600

    Advent of Code 2024 - Day One

---
+++ src/test/kotlin/day1/DayOneTest.kt
@@ -0,0 +1,22 @@
+package day1
+
+import day1.DayOneClass
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DayOneTest {
+
+    private val dayOneTest: DayOneClass = DayOneClass()
+
+    @Test
+    fun testDayOnePartOne() {
+     val expected = 11
+     assertEquals(expected, dayOneTest.dayOnePartOne("C:\\github\\aoc-2024\\src\\test\\kotlin\\day1\\DayOneTest.txt"))
+    }
+
+    @Test
+    fun testDayOnePartTwo() {
+     val expected = 31
+     assertEquals(expected, dayOneTest.dayOnePartTwo("C:\\github\\aoc-2024\\src\\test\\kotlin\\day1\\DayOneTest.txt"))
+    }
+}
